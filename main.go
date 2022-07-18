c0566691d4f56f4915a0ee83b73ccb8ddadd84ba
Asi Baka <39007861+asabaki@users.noreply.github.com>
2022-07-18 22:03:18 +0700

    first init

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	var whatIsIt string
+
+	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+	sd, _ := base64.StdEncoding.DecodeString(secret)
+	whatIsIt = reverse(sd)
+	fmt.Println(whatIsIt)
+}
+
+func reverse(s []byte) string {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return string(s)
+}
